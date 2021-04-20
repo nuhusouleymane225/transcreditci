5787fb05ad9b610892436e38b685d7328e239ac6
NUHU <souleymaneeladjnuhu@gmail.com>
2021-04-20 07:54:40 +0000

    rebase

--- Application/app/views.py
+++ Application/app/views.py
@@ -1,44 +1,46 @@
-from django.shortcuts import render, redirect
-from .forms import PersonalDataForm
-from .models import PersonalData, Commentaire, Newsletter
-# Create your views here.
-from django.contrib import messages
+"""
+Definition of views.
+"""
+
+from django.shortcuts import render
+from django.http import HttpRequest
+from django.template import RequestContext
+from datetime import datetime
 
 def home(request):
-    return render(request, 'pages/index.html')
-
-
-def about(request):
-    return render(request, 'pages/about-us.html')
-
+    """Renders the home page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/index.html',
+        {
+            'title':'Home Page',
+            'year':datetime.now().year,
+        }
+    )
 
 def contact(request):
-    return render(request, 'pages/contact-us.html')
+    """Renders the contact page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/contact.html',
+        {
+            'title':'Contact',
+            'message':'Your contact page.',
+            'year':datetime.now().year,
+        }
+    )
 
-
-def vehicule(request):
-    return render(request, 'pages/vehicule.html')
-
-
-def vehicule_detail(request):
-    return render(request, 'pages/shop-detail.html')
-
-
-def formulaire( request):
-     form = PersonalDataForm(request.POST or None)
-     if form.is_valid():
-         obj=PersonalData.objects.create(** form.cleaned_data)
-         obj.save()
-         form = PersonalDataForm()
-         print('data valid')
-         messages.success(request, "Votre dossier a été soumis avec succes, vous allez être recontacté!")
-     else: 
-         print('data is not valid')
-         messages.warning(request, "Une erreur s'est produite!")
-     context={'form': form}
-     template_name = 'pages/formulaire.html'
-     return render(request, template_name, context)
-
-
-def details(request):
-    return render(request, 'pages/shop-detail.html')
+def about(request):
+    """Renders the about page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/about.html',
+        {
+            'title':'About',
+            'message':'Your application description page.',
+            'year':datetime.now().year,
+        }
+    )
--- Application/python_webapp_django/settings.py
+++ Application/python_webapp_django/settings.py
@@ -87,7 +87,7 @@ APPLICATION_INSIGHTS = {
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -151,7 +151,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
+STATIC_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ['static']))
